684ceb16720d6c7e9963653fc71f798ac03caa9e
Diego Fabiano <dfabiano@mail.usf.edu>
2018-11-19 17:10:33 -0500

    raw images reader and normalizer

--- scripts/file_operations.py
+++ scripts/file_operations.py
@@ -22,7 +22,7 @@ def read_images_together(images_dir= '../data/faces/Training/',
             frame_number = int(experiment.split('.')[1].split('e')[-1]) - 1
             valence = y_values[frame_number]
                 
-            x.append(imread(destination,1))
+            x.append(cv2.imread(destination,1))
             y.append(valence)
                 
     return np.asarray(x), np.asarray(y)
@@ -50,7 +50,7 @@ def read_images_subject(subject_list, story_list,
             frame_number = int(experiment.split('.')[1].split('e')[-1]) - 1
             valence = y_values[frame_number]
                 
-            x[subject][story].append(imread(destination,1))
+            x[subject][story].append(cv2.imread(destination,1))
             y[subject][story].append(valence)
                 
     return np.asarray(x), np.asarray(y)
@@ -79,6 +79,9 @@ def read_all_data(subject_list,story_list, data_directory = '../data/results/'):
 	
 def readImage(path):
 	return (cv2.imread(path[0],cv2.IMREAD_GRAYSCALE) / 255, path[1])
+
+def readImage_rgb(path):
+	return (cv2.imread(path[0]), path[1])
         
 def read_landmark_images(data_directory='../data/Images/'):
 	file_list = []
@@ -91,3 +94,61 @@ def read_landmark_images(data_directory='../data/Images/'):
 	with Pool() as p:
 		images = p.map(readImage, file_list)
 		return np.asarray([items[0] for items in sorted(images, key=lambda x: x[1])])
+
+def read_raw_images(data_directory,subject_list, story_list, y_directory):
+    data_directory = ''
+    subject_list = [1,2,3,4,5,6,7,8,9,10]
+    story_list = [2,4,5,8]
+    
+    file_list = [file for file in os.listdir(data_directory) if file.endswith('.png')]
+    y_file_list = [file for file in os.listdir(y_directory) if file.endswith('.png')]
+    
+    subject_container = [[[] for story in range(0,len(story_list))] for subject in range(0,len(subject_list))]
+    actor_container = [[[] for story in range(0,len(story_list))] for subject in range(0,len(subject_list))]
+    
+    # Get Images
+    for file in file_list:
+        f = data_directory + file
+        image = cv2.imread(f)
+        subject = int(f.split('/')[-1].split('_')[1])
+        story = int(f.split('/')[-1].split('_')[3])
+        frame = int(f.split('/')[-1].split('_')[5])
+        
+        person = f.split('/')[-1].split('_')[-1].split('.')[0]
+        
+        if person is 'actor':    
+            actor_container[subject][story].append((image,frame))
+        else:
+            subject_container[subject][story].append((image,frame))
+
+    # Arrange images in right order
+    for subject in range(0,len(subject_list)):
+        for story in range(0,len(story)):
+            subject_container = sorted(subject_container[subject][story], key=lambda x: x[1])
+            actor_container = sorted(actor_container[subject][story], key=lambda x: x[1])
+    
+    
+    # Get valence levels
+    y_container = [[[] for story in range(0,len(story_list))] for subject in range(0,len(subject_list))]    
+    for file in y_file_list:
+        f = y_directory + file
+        subject = int(f.split('/')[-1].split('_')[1])
+        story = int(f.split('/')[-1].split('_')[-1].split('.')[0])
+        y_container[subject][story] = pd.read_csv(f, index_col=None)['valence'].values
+        
+    subject_x,actor_x,y = [], [], []
+    # Order everything in one big container
+    for subject in range(0,len(subject_list)):
+        for story in range(0,len(story)):
+            for index in range(0,len(actor_container[subject][story])):
+                subject_x.append(subject_container[subject][story][index][0])
+                actor_x.append(actor_container[subject][story][index][0])
+                y.append(y_container[subject][story][index])                
+                
+    # Scale Images
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler(copy =False)
+    subject_x = scaler.fit_transform(subject_x)
+    actor_x = scaler.fit_transform(actor_x)
+    
+    return subject_x,actor_x, y
