5e8e4eff67f17507b277170653d8738f08b0ded2
Neil Sambhu <neilsambhu@gmail.com>
2018-11-20 15:55:45 -0500

    splitting audio data

--- scripts/extract_audio.py
+++ scripts/extract_audio.py
@@ -21,15 +21,19 @@ def extractAudio(path, savePath):
 
     for video in videos:
         videoPath = path + "/" + video
+        videoNoExtension = os.path.splitext(video)[0]
 
         if not os.path.exists(savePath):
             print ("- Processing Video:", videoPath + " ...")
             os.makedirs(savePath)
 
         #copyfile(videoPath, copyTargetVideo)
-        print ("--- Extracting audio:", savePath + "/" + video + ".wav" + " ...")
+        print ("--- Extracting audio:", savePath + "/" + videoNoExtension + ".wav" + " ...")
 
-        command1 = 'ffmpeg -v quiet -i {0} -ar 125000 -ac 1 -vn {1}'.format(videoPath, savePath + "/" + video + ".wav")
+		# original
+        command1 = 'ffmpeg -v quiet -i "{0}" -ac 1 -vn "{1}"'.format(videoPath, savePath + "/" + videoNoExtension + ".wav")
+		# downsampled
+        #command1 = 'ffmpeg -v quiet -i {0} -ar 125000 -ac 1 -vn {1}'.format(videoPath, savePath + "/" + videoNoExtension + ".wav")
         subprocess.call(command1, shell=True)
 
 
@@ -41,20 +45,20 @@ if __name__ == "__main__":
     # Path where the videos are
     #path = "/data/Validation/Videos/"
     #path = "D:\\Neil_TFS\\AR Emotion Research\\OMG-Empathy-Challenge\\omg-empathy\\data\\Training\\Videos\\"
-    #path = "/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/Training/Videos/"
-    path = "/media/neil/30CA58BFCA588350/data/Training/Videos/"
+    path = '/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/Training/Videos/'
+    #path = "/media/neil/30CA58BFCA588350/data/Training/Videos/"
     #path = "../data/Validation/Videos"
 
     # Path where the audios will be saved
     #savePath ="/data/audio/Validation/"
     #savePath = "D:\\Neil_TFS\\AR Emotion Research\\OMG-Empathy-Challenge\\omg-empathy\\data\\audio-reduced\\Training\\"
-    #savePath = "/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-reduced/Training/"
-    savePath = "/media/neil/30CA58BFCA588350/data/audio-reduced/Training/"
+    savePath = '/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio/Training/'
+    #savePath = "/media/neil/30CA58BFCA588350/data/audio-reduced/Training/"
     #savePath = "../data/audio/Validation"
 
     extractAudio(path,savePath)
 
     print('Starting extracting Validation audio')
-    path = "/media/neil/30CA58BFCA588350/data/Validation/Videos/"
-    savePath = "/media/neil/30CA58BFCA588350/data/audio-reduced/Validation/"
+    path = '/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/Validation/Videos/'
+    savePath = '/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio/Validation/'
     extractAudio(path,savePath)
