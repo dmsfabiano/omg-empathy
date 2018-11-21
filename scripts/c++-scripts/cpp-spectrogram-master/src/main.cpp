7987bf3fdc6fe9859aadbff17352710c5f84416d
Neil Sambhu <neilsambhu@gmail.com>
2018-11-21 18:25:47 -0500

    parallel spectrogram, work in progress

--- scripts/c++-scripts/cpp-spectrogram-master/src/main.cpp
+++ scripts/c++-scripts/cpp-spectrogram-master/src/main.cpp
@@ -30,6 +30,11 @@ namespace fs = std::experimental::filesystem;
 #include <thread>         // std::thread
 #include <vector>         // std::vector
 
+#include <ctime>
+#include <chrono>
+
+#include <csignal>
+
 void createSpectrographs(const std::string& sParentDirectory, const std::string& sOutputDirectory)
 {
 	for (const auto& p : fs::directory_iterator(sParentDirectory))
@@ -52,34 +57,48 @@ void createSpectrographs(const std::string& sParentDirectory, const std::string&
 	}
 }
 
-void createSingleSpetrograph(const std::string& sWavFile, const std::string& sOutputDirectory)
+void createSingleSpetrograph(const std::string sWavFile, const std::string& sOutputDirectory)
 {
-	Spectrograph spectrograph(sWavFile, 256, 256);
+	try {
+		Spectrograph spectrograph(sWavFile, 256, 256);
 
-	if (!spectrograph.file_is_valid()) {
-		exit(1);
+		if (!spectrograph.file_is_valid()) {
+			exit(1);
+		}
+
+		spectrograph.set_window(Utility::blackman_harris);
+		spectrograph.compute(2048, 0.8);
+
+		// compute output file name
+		size_t firstIndex = sWavFile.find_last_of("/");
+		size_t lastIndex = sWavFile.find_last_of(".");
+		std::string wavFileNoExtension = sWavFile.substr(firstIndex, lastIndex);
+		spectrograph.save_image(sOutputDirectory + wavFileNoExtension + ".png", false);
 	}
-
-	spectrograph.set_window(Utility::blackman_harris);
-	spectrograph.compute(2048, 0.8);
-
-	// compute output file name
-	size_t firstIndex = sWavFile.find_last_of("/");
-	size_t lastIndex = sWavFile.find_last_of(".");
-	std::string wavFileNoExtension = sWavFile.substr(firstIndex, lastIndex);
-	spectrograph.save_image(sOutputDirectory + wavFileNoExtension + ".png", false);
+	catch (const std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
 void createSpectrographsParallel(const std::string& sParentDirectory, const std::string& sOutputDirectory)
 {
-	std::vector<std::thread> threads;
+	try {
+		std::vector<std::thread> threads;
 
-	for (const auto& p : fs::directory_iterator(sParentDirectory))
-	{
-		std::string sWavFile = p.path().string();
-		threads.push_back(std::thread(createSingleSpetrograph, sWavFile, sOutputDirectory));
+		for (const auto& p : fs::directory_iterator(sParentDirectory))
+		{
+			if (p.path().extension() == ".wav")
+			{
+				threads.push_back(std::thread(createSingleSpetrograph, p.path().string(), std::ref(sOutputDirectory)));
+			}
+		}
+
+		for (auto& th : threads) th.join();
 	}
-
-	for (auto& th : threads) th.join();
+	catch (const std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
 
 int main(int argc, const char *argv[])
@@ -92,33 +111,45 @@ int main(int argc, const char *argv[])
 
 	// =====================================================================================
 
-	// parallel code
-	std::cout << "start training" << std::endl;
-	createSpectrographsParallel(
-		"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Training/",
-		"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Training/");
-	std::cout << "end training" << std::endl;
-
-	std::cout << "start validation" << std::endl;
-	createSpectrographsParallel(
-		"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Validation/",
-		"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Validation/");
-	std::cout << "end validation" << std::endl;
+	//// parallel code
+	//std::cout << "start training" << std::endl;
+	//createSpectrographsParallel(
+	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Training/",
+	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Training/");
+	//std::cout << "end training" << std::endl;
+	//std::cout << "start validation" << std::endl;
+	//createSpectrographsParallel(
+	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Validation/",
+	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Validation/");
+	//std::cout << "end validation" << std::endl;
 
 	// =====================================================================================
 
-	//// very parallel code
-	//std::thread training(createSpectrographsParallel,
-	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Training/",
-	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Training/");
-	//std::thread testing(createSpectrographsParallel,
-	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Validation/",
-	//	"/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Validation/");
-	//std::cout << "start training and testing" << std::endl;
+	// very parallel code
+	std::cout << "start training and testing at ";
+	std::time_t timeOutput = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::cout << std::ctime(&timeOutput) << std::endl;
 
-	//// synchronize threads
-	//training.join();
-	//testing.join();
-
-	//std::cout << "end training and testing";
+	try {
+		std::thread training(
+			createSpectrographsParallel,
+			std::move("/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Training/"),
+			std::move("/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Training/")
+		);
+		std::thread testing(
+			createSpectrographsParallel,
+			std::move("/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/audio-split/Validation/"),
+			std::move("/mnt/d/Neil_TFS/AR Emotion Research/OMG-Empathy-Challenge/omg-empathy/data/spectrograms/Validation/")
+		);
+		// synchronize threads
+		training.join();
+		testing.join();
+	}
+	catch (const std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "end training and testing at ";
+	timeOutput = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::cout << std::ctime(&timeOutput) << std::endl;
 }
